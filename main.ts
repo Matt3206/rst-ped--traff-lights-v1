88abda6f36637e82fb858a1317e303921ac89241
Matt3206 <101339805+Matt3206@users.noreply.github.com>
2022-06-14 06:46:56 -0700

    It works now

--- main.ts
+++ main.ts
@@ -1,9 +1,10 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 103) {
+    if (receivedNumber == 1) {
         Green()
-        basic.pause(20000)
-        Yellow()
         basic.pause(10000)
+        Yellow()
+        basic.pause(5000)
+        Red()
     } else {
         Red()
     }
@@ -14,20 +15,23 @@ function Traffic_Lights_Sensor () {
     pins.digitalWritePin(DigitalPin.P1, 1)
     control.waitMicros(10)
     pins.digitalWritePin(DigitalPin.P1, 0)
-    distance = pins.pulseIn(DigitalPin.P0, PulseValue.High) + 58
-    if (distance >= 5) {
-        Green()
-        basic.pause(10000)
-        Yellow()
-        basic.pause(5000)
-    } else if (distance < 5) {
-        Red()
-        basic.pause(1000)
-    }
+    distance = pins.pulseIn(DigitalPin.P2, PulseValue.High) / 58
 }
 input.onButtonPressed(Button.A, function () {
-    Ped += 1
-    Ped_Fun()
+    Ped_countdown = 10
+    if (Car_Check == 0) {
+        basic.showIcon(IconNames.StickFigure)
+        Green()
+        basic.pause(10000)
+        for (let index = 0; index <= 9; index++) {
+            basic.showNumber(Ped_countdown - index)
+        }
+    }
+    basic.pause(1000)
+    basic.showIcon(IconNames.No)
+    Yellow()
+    basic.pause(5000)
+    Red()
 })
 function Yellow () {
     range = strip.range(0, 1)
@@ -37,43 +41,35 @@ function Yellow () {
     range = strip.range(2, 1)
     range.showColor(neopixel.colors(NeoPixelColors.Black))
 }
-function Ped_Fun () {
-    Ped_countdown = 5
-    if (Ped == 0) {
-        Red()
-        basic.showIcon(IconNames.No)
-    } else if (Ped == 1) {
-        Green()
-        basic.showIcon(IconNames.StickFigure)
-        music.playMelody("C - - - C - - - ", 120)
-        basic.pause(10000)
-        for (let index = 0; index <= 4; index++) {
-            Yellow()
-            basic.showNumber(Ped_countdown - index)
-        }
-        Ped = 0
-        basic.showIcon(IconNames.No)
-        Red()
-    } else {
-        Ped_countdown += 5
+function TRAFFIC_LIGHT () {
+    basic.showIcon(IconNames.No)
+    Car_Check = 1
+    basic.pause(5000)
+    Green()
+    basic.pause(10000)
+    Yellow()
+    basic.pause(5000)
+    Car_Check = 0
+    Red()
+}
+input.onButtonPressed(Button.B, function () {
+    Ped_countdown = 10
+    if (Car_Check == 0) {
         Green()
         basic.showIcon(IconNames.StickFigure)
         for (let index = 0; index < 3; index++) {
             music.playMelody("C5 B A - C5 B A - ", 120)
         }
         basic.pause(15000)
-        Ped = 0
         for (let index = 0; index <= 9; index++) {
-            Yellow()
             basic.showNumber(Ped_countdown - index)
         }
-        Red()
+        basic.pause(5000)
         basic.showIcon(IconNames.No)
+        Yellow()
+        basic.pause(5000)
+        Red()
     }
-}
-input.onButtonPressed(Button.B, function () {
-    Ped += 2
-    Ped_Fun()
 })
 function Green () {
     range = strip.range(0, 1)
@@ -90,18 +86,29 @@ function Red () {
     range.showColor(neopixel.colors(NeoPixelColors.Black))
     range = strip.range(2, 1)
     range.showColor(neopixel.colors(NeoPixelColors.Black))
+    basic.showIcon(IconNames.No)
 }
-let Ped_countdown = 0
 let range: neopixel.Strip = null
+let Ped_countdown = 0
 let distance = 0
 let strip: neopixel.Strip = null
-let Ped = 0
-Ped = 0
+let Car_Check = 0
+radio.setGroup(103)
+Car_Check = 0
+let Light_Check = 0
 strip = neopixel.create(DigitalPin.P16, 3, NeoPixelMode.RGB)
 strip.setBrightness(255)
-radio.setGroup(103)
-Ped_Fun()
+Red()
 basic.forever(function () {
-    Traffic_Lights_Sensor()
-    Ped_Fun()
+    for (let index = 0; index < 10; index++) {
+        Traffic_Lights_Sensor()
+        if (distance <= 5) {
+            Light_Check += 1
+        }
+    }
+    if (Light_Check == 9 || Light_Check == 10) {
+        TRAFFIC_LIGHT()
+        Light_Check = 0
+    }
+    Light_Check = 0
 })
